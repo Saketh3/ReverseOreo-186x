c221b2f6bead9ab6df4469c4e0e822417592251f
Paul Brinkmann <pbrink21@live.com>
2020-01-29 14:13:48 -0600

    added mc mod code

---
+++ src/main/java/com/oerOesreveR/cheese/cheese.java
@@ -0,0 +1,10 @@
+package com.oerOesreveR.cheese;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.oerOesreveR.cheese.cheese.MOD_ID;
+
+@Mod(MOD_ID)
+public class cheese {
+    public static final String MOD_ID = "cheese";
+}
