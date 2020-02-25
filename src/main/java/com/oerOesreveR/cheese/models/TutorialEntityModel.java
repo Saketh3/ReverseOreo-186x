665210f1baa8ad38549feae29d779822fbd79b4b
Saketh Jonnadula <59926413+Saketh3@users.noreply.github.com>
2020-02-25 16:37:23 -0600

    cow

--- src/main/java/com/oerOesreveR/cheese/models/TutorialEntityModel.java
+++ src/main/java/com/oerOesreveR/cheese/models/TutorialEntityModel.java
@@ -1,11 +1,14 @@
 package com.oerOesreveR.cheese.models;
 
 import com.oerOesreveR.cheese.TutorialEntity;
+import net.minecraft.client.renderer.entity.model.CowModel;
 import net.minecraft.client.renderer.entity.model.PigModel;
+import net.minecraft.client.renderer.entity.model.PillagerModel;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class TutorialEntityModel extends PigModel<TutorialEntity> {
+public class TutorialEntityModel extends CowModel<TutorialEntity> {
+
 
 }
