aa62d919d68c223cdb540224fe5316f5bfc282ad
Saketh Jonnadula <59926413+Saketh3@users.noreply.github.com>
2020-03-04 09:36:57 -0600

    Added FollowOwnerGoal

--- src/main/java/com/oerOesreveR/cheese/TutorialEntity.java
+++ src/main/java/com/oerOesreveR/cheese/TutorialEntity.java
@@ -4,15 +4,15 @@ import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.brain.task.WalkRandomlyTask;
-import net.minecraft.entity.ai.goal.FollowMobGoal;
-import net.minecraft.entity.ai.goal.FollowOwnerGoal;
-import net.minecraft.entity.ai.goal.RandomWalkingGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 
 public class TutorialEntity extends CreatureEntity {
+
+
 
     public TutorialEntity(EntityType<? extends CreatureEntity> type, World worldIn) {
         super((EntityType<? extends CreatureEntity>) TutorialEntities.TUTORIAL_ENTITY, worldIn);
@@ -22,7 +22,9 @@ public class TutorialEntity extends CreatureEntity {
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(1, new RandomWalkingGoal(this, .4));
-        this.goalSelector.addGoal(2, new FollowMobGoal(this, PigEntity, 1.0D, 3.0F, 7.0F));
+        this.goalSelector.addGoal(2, new FollowMobGoal(this, 1.0D, 3.0F, 7.0F));
+        this.goalSelector.addGoal(3,new FollowOwnerGoal(this,1.0D,10.0F,2.0F));
+        this.goalSelector.addGoal(4, new LookAtGoal(this, PlayerEnitiy.class,8.0f);
     }
 
     @Override
@@ -31,4 +33,5 @@ public class TutorialEntity extends CreatureEntity {
         this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(2.0d);
         this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(1.4d);
     }
+
 }
